1b5662548dca04478f4b5a9979ff8541bc59aea1
Maurício Marcos <mauricio.marcos@castgroup.com.br>
2021-06-24 20:37:17 -0300

    Iniciando código no GITHUB

---
+++ Lab02-Desafio02/Classes/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Lab02_Desafio02.Classes
+{
+    public class Animal
+    {
+        private string[] tiposValidos = {"CACHORRO", "GATO", "PEIXE"};
+        private string tipo;
+
+        public Animal(string nome, string tipo)
+        {
+            Nome = nome;
+            Tipo = tipo;
+        }
+
+        public string Nome { get; private set; }
+
+        public string Tipo 
+        {
+            get 
+            {
+                return this.tipo;
+            }
+            set 
+            {
+                if (tiposValidos.Contains(value))
+                    this.tipo = value;
+                else
+                    this.tipo = "Animal não identificado";
+            } 
+        }
+
+        public string Especie { get; set; }
+    }
+}
---
+++ Lab02-Desafio02/Program.cs
@@ -0,0 +1,65 @@
+﻿using Lab02_Desafio02.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace Lab02_Desafio02
+{
+    class Program
+    {
+        private static int totalCachorro, totalGato, totalPeixe, totalDesconhecido;
+
+        static void Main()
+        {
+            var animais = CriarAnimais();
+            ComputarAnimais(animais);
+            ExibirTotais();
+        }
+
+        private static List<Animal> CriarAnimais()
+        {
+            return new List<Animal>()
+            {
+                new Animal("Totó", "Cachorro".ToUpper()),
+                new Animal("Miau", "Gato".ToUpper()),
+                new Animal("Roar", "Leão".ToUpper()),
+                new Animal("Miau2", "Gato".ToUpper()),
+                new Animal("Miau3", "Gato".ToUpper()),
+                new Animal("Panda", "Urso".ToUpper()),
+                new Animal("Panda2", "Urso".ToUpper()),
+                new Animal("Nemo", "Peixe".ToUpper()),
+                new Animal("Nemo1", "Peixe".ToUpper()),
+                new Animal("Nemo2", "Peixe".ToUpper())
+            };
+        }
+
+        private static void ComputarAnimais(List<Animal> animais)
+        {
+            foreach (var animal in animais)
+            {
+                switch (animal.Tipo)
+                {
+                    case "CACHORRO":
+                        totalCachorro++;
+                        break;
+                    case "GATO":
+                        totalGato++;
+                        break;
+                    case "PEIXE":
+                        totalPeixe++;
+                        break;
+                    default:
+                        totalDesconhecido++;
+                        break;
+                }
+            }
+        }
+
+        private static void ExibirTotais()
+        {
+            Console.WriteLine($"Total de Cahorros: { totalCachorro }");
+            Console.WriteLine($"Total de Gatos: { totalGato }");
+            Console.WriteLine($"Total de Peixes: { totalPeixe }");
+            Console.WriteLine($"Total de Animais Desconhecidos: { totalDesconhecido }");
+        }
+    }
+}
